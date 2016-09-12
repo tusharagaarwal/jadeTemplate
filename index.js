d838eb37ec2e37f1ed48ced0d7fe270cc86e1c13
Tushar Agarwal <tusharagaarwal@gmail.com>
2016-09-12 13:27:17 +0530

    Template finished
    -Sessions done
    - Login/logout redirect
    - Request handlers done
    - Data structures might require some tweaking based on the application.
    
    Kindly branch and make the changes. Do not make any changes in the master unless it improves on the functionality of the template. Do not make any <application> based changes in the master.
    
    Thanks
    tusharagaarwal

--- index.js
+++ index.js
@@ -1,50 +1,113 @@
 var
-jade = require('jade'),
-app = require('express')(),
-fs = require('fs'),
-url = require('url');
+	jade = require('jade'),
+	app = require('express')(),
+	fs = require('fs'),
+	url = require('url'),
+	clientSessions = require('client-sessions'),
+	querystring = require('querystring'),
+	path = require('path');
 
-app.get('/', function(req, res){
+app.use(clientSessions({
+	secret: 'thisshouldbesomethingserious'
+}));
+	
+function loadPage(contentObject, message, req, res) {
 	var renderFunc = jade.compileFile('./template_index.jade');
 	var content = '';
-	fs.readFile('./data.json', 'utf-8', function(err, data){
+	fs.readFile('./data/data.json', 'utf-8', function(err, data){
 		content = JSON.parse(data);
-		data = content.index;
-		var html = renderFunc(data);
-		res.writeHead("200", {"content-type": "text/html"});
-		res.write(html);
-		res.end();
+		content = content[contentObject];
+		if(content){
+			fs.readFile(content.contentPage, function(err, data){
+				if(err) { /*load error page */console.log(Date()+": "+err);}
+				else{
+					content.message = message;
+					content.username = req.session_state.username;
+					content.content = data;	
+					var html = renderFunc(content);
+					res.writeHead("200", {"content-type": "text/html"});
+					res.write(html);
+					res.end();
+				}
+			});
+		} else {
+			// in case the page object does not exist
+			loadPage("404", "", req, res);
+		}
 	});
-	
+}
+
+function checkUserSession(req, res, pagename, message) {
+	if(req.session_state.username) {
+		// session exists
+		loadPage(pagename, message, req, res);
+	}
+	else {
+		// session not there, redirect to login
+		loadPage("login", "<div class='alert alert-warning alert-dismissible' role='alert' style='margin:0; display:: block;'>No Login found</div>", req, res);
+	}
+}
+
+app.get('/', function(req, res){
+	/*
+	need tto add distinct redirects based on roles
+	*/
+	checkUserSession(req, res, "index", "");
 });
 
 app.get('/login', function(req, res){
-	var renderFunc = jade.compileFile('./template_index.jade');
-	var content = '';
-	fs.readFile('./data.json', 'utf-8', function(err, data){
-		content = JSON.parse(data);
-		data = content.login;
-		var html = renderFunc(data);
-		res.writeHead("200", {"content-type": "text/html"});
-		res.write(html);
-		res.end();
-	});
-	// can also be written as 
-	// fs.readFile('./index.jade',function(err, sourse){
-	// var html = jade.render(source, {pageTitle:'login', and other data});});
-	// or
-	// fs.readFile('./index.jade',function(err, sourse){
-	// var template = jade.compile(souce);
-	// var html = template(data);});
-	// or
-	// jade.renderFile('./index.jade', data, function(err, html){
-	// console.log(html);});
+	if(req.session_state.username) {
+		loadPage("index", "<div class='alert alert-success alert-dismissible' role='alert' style='margin:0; display:: block;'>Welcome back " + req.session_state.username + "</div>", req, res);
+	}
+	else {
+		loadPage("login", "<div class='alert alert-info alert-dismissible' role='alert' style='margin:0; display:: block;'>Please login to continue</div>", req, res);
+	}
 });
 
-app.get('*', function(req, res) {
+app.get('/logout', function(req, res) {
+	req.session_state.reset();
+	loadPage("login", "<div class='alert alert-success alert-dismissible' role='alert' style='margin:0; display:: block;'>User logged out. Login to continue</div>", req, res);
+});
+
+app.post('/login', function(req, res) {
+	var logindata = "";
+	req.on('data', function(chunk) {
+		logindata += chunk;
+	});
+	req.on('end', function() {
+		logininfo = querystring.parse(logindata);
+		var username = logininfo['username'];
+		var password = logininfo['password'];
+		fs.readFile('./data/users.json', function(ERR, data) {
+			if(ERR) {/*redirect to error page */Date()+": "+console.log(ERR);}
+			else {
+				var userdb = JSON.parse(data);
+				var userinfo = userdb.users[username];
+				if(userinfo) {
+					if(userinfo.password === password) {
+						// start session and redirect
+						req.session_state.username = username;
+						req.session_state.role = userinfo.role;
+						loadPage("index", "<div class='alert alert-success alert-dismissible' role='alert' style='margin:0; display:: block;'>Welcome back " + req.session_state.username + "</div>", req, res);
+					} else {
+						loadPage("login", "<div class='alert alert-danger alert-dismissible' role='alert' style='margin:0; display:: block;'>Invalid Password</div>", req, res);
+					}
+				} else {
+					loadPage("login", "<div class='alert alert-danger alert-dismissible' role='alert' style='margin:0; display:: block;'>Invalid username!</div>", req, res);
+				}
+			}
+		});
+	});
+});
+
+app.get(['*.css', '*.js'], function(req, res) {
 	var pathname = url.parse(req.url).pathname;
 	fs.exists('./'+pathname, function(exists) {
-		console.error(exists? itexists() : doesnot());
+		if(exists) {
+			itexists()
+		} else {
+			doesnot()
+		}
 	});
 	function itexists() {
 		rstream = fs.createReadStream('./'+pathname);
@@ -54,6 +117,11 @@ app.get('*', function(req, res) {
 		content = "404 not found";
 		res.end(content);
 	}
+});
+
+app.get('*', function(req, res) {
+	var pathname = url.parse(req.url).pathname;
+	checkUserSession(req, res, pathname, "");
 });
 
 try{
